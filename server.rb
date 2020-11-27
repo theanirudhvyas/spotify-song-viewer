7ec6d6b2f8a35b01121e44dbd373f5deb76cfe65
Anirudh <anirudh2403@gmail.com>
2020-11-27 19:47:49 +0530

    Fixes bug in response

--- server.rb
+++ server.rb
@@ -56,7 +56,7 @@ class Server < Sinatra::Base
                           headers: { 'Authorization' => "Bearer #{@tokens[:access]}"})
 
     if response.success?
-      return response.status, [], { success: true, data: {playlists: response.body}, errors: []}
+      return response.code, [], { success: true, data: {playlists: response.body}, errors: []}
     elsif response.unauthorized?
       @log.info "The response is unauthorized, refreshing tokens"
       response = refresh_access_token
